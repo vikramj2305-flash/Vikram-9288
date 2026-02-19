fdfbfc77ac18653b0a687ef129a4ec4445acaa87
vikramj2305-flash <vikramj2305@gmail.com>
2026-02-19 09:20:17 +0530

    Create Print Array 6.c

---
+++ Print Array 6.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    
+    
+    scanf("%d", &n);
+    
+    int arr[n];
+    
+    
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    
+    for(int i = 0; i < n; i++) {
+        printf("%d", arr[i]);
+        if(i < n - 1) {
+            printf(" ");
+        }
+    }
+    
+    return 0;
+}
