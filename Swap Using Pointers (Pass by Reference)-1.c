0f992e3facd8abc797cf6a57e03af6fe5b31b656
vikramj2305-flash <vikramj2305@gmail.com>
2026-02-19 15:09:36 +0530

    Create  Swap Using Pointers (Pass by Reference)-1.c

---
+++ Swap Using Pointers (Pass by Reference)-1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int x, y;
+    scanf("%d %d", &x, &y);
+    swap(&x, &y);
+    printf("After Swap: %d %d\n", x, y);
+    return 0;
+}
