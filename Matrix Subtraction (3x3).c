730af65a6a2938e7c021459b858705d7d3ba3e4c
vikramj2305-flash <vikramj2305@gmail.com>
2026-02-19 15:17:18 +0530

    Create Matrix Subtraction (3x3).c

---
+++ Matrix Subtraction (3x3).c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int matrix1[3][3], matrix2[3][3], result[3][3];
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            scanf("%d", &matrix1[i][j]);
+        }
+    }
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            scanf("%d", &matrix2[i][j]);
+        }
+    }
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            result[i][j] = matrix1[i][j] - matrix2[i][j];
+        }
+    }
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d", result[i][j]);
+            if (j < 2) printf(" ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
